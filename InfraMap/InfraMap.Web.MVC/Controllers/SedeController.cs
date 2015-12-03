07ca4c2db73f5fcd9288fa42cf3ca955dddfd5c8
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-12-03 16:24:58 -0200

    adição tela de sede

---
+++ InfraMap/InfraMap.Web.MVC/Controllers/SedeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfraMap.Web.MVC.Controllers
+{
+    public class SedeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InfraMap/InfraMap.Web.MVC/Models/SedeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraMap.Web.MVC.Models
+{
+    public class SedeModel
+    {
+        public int IdSede { get; set; }
+
+        public List<AndarModel> ListaAndares { get; set; }
+
+        public SedeModel()
+        {
+            this.ListaAndares = new List<AndarModel>();
+        }
+    }
+}
