ed5b93a197084d4a46ba8f9330184a4956ad7f16
emmanoelf <emmanoel_95@hotmail.com>
2015-12-03 01:32:04 -0200

    feat(BundeConfig) adicionado modernizr e jquery-validation

--- InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
+++ InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
@@ -13,12 +13,19 @@ namespace InfraMap.Web.MVC.App_Start
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                         "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
                         "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.unobtrusive*",
+                    "~/Scripts/jquery.validate*"));
         }
     }
 }
