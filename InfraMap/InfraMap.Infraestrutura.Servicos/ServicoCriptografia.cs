2b7875c298b0447f1879d79423c84c50cae6fdbb
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-12-03 14:40:59 -0200

    implementado criptografia de senha

--- InfraMap/InfraMap.Infraestrutura.Servicos/ServicoCriptografia.cs
+++ InfraMap/InfraMap.Infraestrutura.Servicos/ServicoCriptografia.cs
@@ -11,7 +11,20 @@ namespace InfraMap.Infraestrutura.Servicos
     {
         public string CriptografarSenha(string senha)
         {
-            throw new NotImplementedException();
+            return PegarHashMd5(senha) + "INFRAMAP";
+        }
+
+        public string PegarHashMd5(string senha)
+        {
+            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();            
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(senha);         
+            byte[] hash = md5.ComputeHash(inputBytes);           
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();           
+            for (int i = 0; i < hash.Length; i++)               
+            {               
+            sb.Append(hash[i].ToString("X2"));
+            }           
+            return sb.ToString();
         }
     }
 }
