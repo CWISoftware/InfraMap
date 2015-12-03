d4868f33dd3f911f032149dffc1b323a072f935d
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-12-02 23:32:19 -0200

    adição das classes autorizador,controle de sessão e usuario logado.

---
+++ InfraMap/InfraMap.Web.MVC/Seguranca/Autorizador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace InfraMap.Web.MVC.Seguranca
+{
+    public class Autorizador : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            UsuarioLogado usuario = ControleDeSessao.UsuarioLogado;
+
+            if (usuario != null && AuthorizeCore(filterContext.HttpContext))
+            {
+                GenericIdentity myIdentity = new GenericIdentity(usuario.Login);
+                GenericPrincipal principal = new GenericPrincipal(myIdentity, usuario.Permissoes);
+
+                Thread.CurrentPrincipal =
+                    HttpContext.Current.User = principal;
+
+                base.OnAuthorization(filterContext);
+            }
+            else
+            {
+                RedirecionarParaLogin(filterContext);
+            }
+        }
+
+
+        private void RedirecionarParaLogin(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                                                new RouteValueDictionary
+                                                {
+                                                    { "action", "Index" },
+                                                    { "controller", "Login" }
+                                                });
+        }
+    }
+}
---
+++ InfraMap/InfraMap.Web.MVC/Seguranca/UsuarioLogado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraMap.Dominio.ModuloUsuario;
+
+namespace InfraMap.Web.MVC.Seguranca
+{
+    public class UsuarioLogado
+    {
+        public string Login { get; private set; }
+
+        public string[] Permissoes { get; private set; }
+
+        public UsuarioLogado(Usuario usuario)
+        {
+            this.Login = usuario.Login;
+            this.Permissoes = usuario.Permissoes.Select(p => p.Texto).ToArray();
+        }
+
+        public bool TemPermissao(string nomePermissao)
+        {
+            return this.Permissoes != null
+                && this.Permissoes.Contains(nomePermissao);
+        }
+
+    }
+}
