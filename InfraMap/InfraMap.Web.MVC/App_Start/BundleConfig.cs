3f05e5dc36bf66ec9bab01e7060a3534bbc94f68
emmanoelf <emmanoel_95@hotmail.com>
2015-12-04 12:16:51 -0200

    feat(InfraMap) adicionado typeahead e parte do script autocomplete

--- InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
+++ InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
@@ -11,8 +11,7 @@ namespace InfraMap.Web.MVC.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
@@ -30,6 +29,7 @@ namespace InfraMap.Web.MVC.App_Start
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                     "~/Scripts/jquery.unobtrusive*",
                     "~/Scripts/jquery.validate*"));
+                    
         }
     }
 }
