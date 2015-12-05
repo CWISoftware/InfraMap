4a89978cf3a8c1586eb7795c6469f826c328b60e
ericgottschalk <ericng04@hotmail.com>
2015-12-05 13:19:18 -0200

    Adicionar migration

---
+++ InfraMap/InfraMap.Infra.Ef/Migrations/201512051517244_Fix.cs
@@ -0,0 +1,26 @@
+namespace InfraMap.Infraestrutura.Ef.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Fix : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Andar", "Sede_Id", "dbo.Sede");
+            DropIndex("dbo.Andar", new[] { "Sede_Id" });
+            AlterColumn("dbo.Andar", "Sede_Id", c => c.Int());
+            CreateIndex("dbo.Andar", "Sede_Id");
+            AddForeignKey("dbo.Andar", "Sede_Id", "dbo.Sede", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Andar", "Sede_Id", "dbo.Sede");
+            DropIndex("dbo.Andar", new[] { "Sede_Id" });
+            AlterColumn("dbo.Andar", "Sede_Id", c => c.Int(nullable: false));
+            CreateIndex("dbo.Andar", "Sede_Id");
+            AddForeignKey("dbo.Andar", "Sede_Id", "dbo.Sede", "Id", cascadeDelete: true);
+        }
+    }
+}
