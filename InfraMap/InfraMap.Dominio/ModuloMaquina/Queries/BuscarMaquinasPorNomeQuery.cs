de50113b384d3e884fccaef6cff43921813bb7e1
ericgottschalk <ericng04@hotmail.com>
2015-12-05 18:52:14 -0200

    Adicionar queries

---
+++ InfraMap/InfraMap.Dominio/ModuloMaquina/Queries/BuscarMaquinasPorNomeQuery.cs
@@ -0,0 +1,24 @@
+﻿using InfraMap.Comum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraMap.Dominio.ModuloMaquina.Queries
+{
+    public class BuscarMaquinasPorNomeQuery : IQuery<Maquina>
+    {
+        private readonly string nomeMaquina;
+
+        public BuscarMaquinasPorNomeQuery(string nomeMaquina)
+        {
+            this.nomeMaquina = nomeMaquina;
+        }
+
+        public IQueryable<Maquina> CriarQuery(IQueryable<Maquina> src)
+        {
+            return src.Where(t => t.Nome.StartsWith(this.nomeMaquina));
+        }
+    }
+}
---
+++ InfraMap/InfraMap.Dominio/ModuloUsuario/Queries/BuscarUsuariosPorNomeQuery.cs
@@ -0,0 +1,24 @@
+﻿using InfraMap.Comum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraMap.Dominio.ModuloUsuario.Queries
+{
+    public class BuscarUsuariosPorNomeQuery : IQuery<Usuario>
+    {
+        private readonly string nome;
+
+        public BuscarUsuariosPorNomeQuery(string nome)
+        {
+            this.nome = nome;        
+        }
+
+        public IQueryable<Usuario> CriarQuery(IQueryable<Usuario> src)
+        {
+            return src.Where(t => t.Nome.StartsWith(this.nome));
+        }
+    }
+}
