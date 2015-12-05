17bda46758ed0a33af11827c609cdf309f0c4ff8
ericgottschalk <ericng04@hotmail.com>
2015-12-04 23:54:10 -0200

    Adicionar models de Maquina, Ramal e Mesa.

---
+++ InfraMap/InfraMap.Web.MVC/Models/MesaModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfraMap.Web.MVC.Models
+{
+    public class MesaModel
+    {
+        public int Id { get; set; }
+
+        public SedeModel Sede { get; set; }
+
+        public MaquinaModel Maquina { get; set; }
+
+        public RamalModel Ramal { get; set; }
+
+        public UsuarioModel Colaborador { get; set; }
+
+        public bool TemMaquina
+        {
+            get
+            {
+                return this.Maquina != null;
+            }
+        }
+
+        public bool TemRamal
+        {
+            get
+            {
+                return this.Ramal != null;
+            }
+        }
+
+        public bool TemColaborador
+        {
+            get
+            {
+                return this.Colaborador != null;
+            }
+        }
+    }
+}
---
+++ InfraMap/InfraMap.Web.MVC/Models/RamalModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfraMap.Web.MVC.Models
+{
+    public class RamalModel
+    {
+        public int Id { get; set; }
+
+        public String Tipo { get; set; }
+
+        public String Numero { get; set; }
+    }
+}
