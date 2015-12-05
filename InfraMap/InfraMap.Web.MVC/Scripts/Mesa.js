1644f42f168f9bdbada90dcae44c62abc09b06bc
ericgottschalk <ericng04@hotmail.com>
2015-12-05 12:33:27 -0200

    feat: AdicionarMaquina e AdicionarRamal.

--- InfraMap/InfraMap.Web.MVC/Scripts/Mesa.js
+++ InfraMap/InfraMap.Web.MVC/Scripts/Mesa.js
@@ -9,3 +9,27 @@
         success: function (data) {}
     });
 });
+
+$("#adicionaMaquina").click(function () {
+    var idMesa = 1;
+    var idMaquina = $("#maquina").val();
+    $.ajax({
+        type: "POST",
+        url: "/SaoLeopoldo/MesaAdicionarColaborador",
+        data: { id: idMesa, maquina: idMaquina },
+        datatype: "json",
+        success: function (data) { }
+    });
+});
+
+$("#adicionaRamal").click(function () {
+    var idMesa = 1;
+    var idRamal = $("#ramal").val();
+    $.ajax({
+        type: "POST",
+        url: "/SaoLeopoldo/MesaAdicionarColaborador",
+        data: { id: idMesa, ramal: idRamal },
+        datatype: "json",
+        success: function (data) { }
+    });
+});
