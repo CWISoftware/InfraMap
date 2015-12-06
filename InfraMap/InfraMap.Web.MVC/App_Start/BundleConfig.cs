6bbdab879727a724a20de548c769a32024267607
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-12-06 19:21:19 -0200

    atualizado gerente escolher colaboradores

--- InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
+++ InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace InfraMap.Web.MVC.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
@@ -19,14 +20,14 @@ namespace InfraMap.Web.MVC.App_Start
             bundles.Add(new ScriptBundle("~/bundles/sede").Include(
                        "~/Scripts/sede.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootstrap.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/mesa").Include(
                         "~/Scripts/Mesa.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/partialMesa").Include(
-                        "~/Scripts/PartialMesa.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui.*"));
+
+            bundles.Add(new StyleBundle("~/bundles/jquery-ui").Include(
+                        "~/Content/jquery-ui.*"));
 
             bundles.Add(new ScriptBundle("~/bundles/andarCss").Include(
                         "~/Content/Andar.css",
@@ -34,7 +35,7 @@ namespace InfraMap.Web.MVC.App_Start
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
-                        "~/Content/site.css"));
+                        "~/Content/Site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                     "~/Scripts/jquery.unobtrusive*",
