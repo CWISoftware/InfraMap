8ec38ff18410342f1630856cf5f4f01e5abfc5cf
ericgottschalk <ericng04@hotmail.com>
2015-12-07 20:26:39 -0200

    Refactor: Remover classes e metodos desnecessarios.

--- InfraMap/InfraMap.Dominio/Mesa/Ramal/IRamalRepositorio.cs
+++ InfraMap/InfraMap.Dominio/Mesa/Ramal/IRamalRepositorio.cs
@@ -5,8 +5,5 @@ namespace InfraMap.Dominio.Mesa.Ramal
 {
     public interface IRamalRepositorio : IRepositorio<Ramal>
     {
-        IList<Ramal> BuscarRamaisPorNumeros(string numero);
-
-        Dominio.Mesa.Ramal.Ramal BuscarRamalPorNumero(string numero);
     }
 }
