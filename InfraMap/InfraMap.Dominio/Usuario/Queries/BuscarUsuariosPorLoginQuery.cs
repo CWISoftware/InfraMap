4933d6d84be7234c0ded7ede431330d36802cbeb
ericgottschalk <ericng04@hotmail.com>
2015-12-06 23:56:13 -0200

    Adicionar BuscarUsuariosPorLoginQuery e implementação no repositorio.

---
+++ InfraMap/InfraMap.Dominio/Usuario/Queries/BuscarUsuariosPorLoginQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraMap.Comum;
+
+namespace InfraMap.Dominio.Usuario.Queries
+{
+    public class BuscarUsuariosPorLoginQuery : IQuery<Usuario>
+    {
+        private string login;
+
+        public BuscarUsuariosPorLoginQuery(string login)
+        {
+            this.login = login;
+        }
+
+        public IQueryable<Usuario> CriarQuery(IQueryable<Usuario> src)
+        {
+            return src.Where(t => t.Login.StartsWith(this.login));
+        }
+    }
+}
