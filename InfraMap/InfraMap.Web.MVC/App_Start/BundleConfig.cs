cd1e0f41be973cbc3c31933c3554fe9b1f3ae568
ericgottschalk <ericng04@hotmail.com>
2015-12-07 10:21:45 -0200

    Corrigir bundle, adicionar /.

--- InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
+++ InfraMap/InfraMap.Web.MVC/App_Start/BundleConfig.cs
@@ -15,7 +15,7 @@ namespace InfraMap.Web.MVC.App_Start
                         "~/Scripts/bootstrap.js",
                         "~/Scripts/jquery.easy-autocomplete.js",
                         "~/Scripts/jquery.easy-autocomplete.min.js",
-                        "~Scripts/base.js"));
+                        "~/Scripts/base.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
