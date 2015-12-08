2d1c59a6079bedf4f4f7198344b12283a214444f
ericgottschalk <ericng04@hotmail.com>
2015-12-08 01:17:27 -0200

    Corrigir conflitos

--- InfraMap/InfraMap.Infraestrutura.Servicos/Properties/AssemblyInfo.cs
+++ InfraMap/InfraMap.Infraestrutura.Servicos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InfraMap.Infraestrutura.Servicos")]
+[assembly: AssemblyTitle("InfraMap.Infraestrutura")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InfraMap.Infraestrutura.Servicos")]
+[assembly: AssemblyProduct("InfraMap.Infraestrutura")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
