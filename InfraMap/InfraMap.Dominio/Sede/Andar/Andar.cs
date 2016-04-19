933b2ca902bc40213e6b9709bed7715549cf5a73
Franklin Amormino <franklin.amormino@cwi.com.br>
2016-04-19 10:03:43 -0300

    Enhancement #6 - Destaque da mesa do usuário pesquisado

--- InfraMap/InfraMap.Dominio/Sede/Andar/Andar.cs
+++ InfraMap/InfraMap.Dominio/Sede/Andar/Andar.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using InfraMap.Dominio.Comum;
 
 namespace InfraMap.Dominio.Sede.Andar
 {
     public class Andar : EntidadeBase
     {
+        private int mesaSelecionada;
+
         public string Descricao { get; set; }
 
         public ICollection<Mesa.Mesa> Mesas { get; set; }
+
+        //Isso foi feito pois o Atributo NotMapping não funciona. Bug no EntityFramework
+        public int GetMesaSelecionada() { return mesaSelecionada; }
+        public void SetMesaSelecionada(int idMesa) { mesaSelecionada = idMesa; }
     }
 }
