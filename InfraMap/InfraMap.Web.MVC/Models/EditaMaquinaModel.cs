bf71a49932f2ebe0f340598af250ffc2f4580bf2
Bruno da Silva Verch <bruno.verch@cwi.com.br>
2016-06-07 13:54:33 -0300

    Adicionado campos das memorias ram nas views de maquina

---
+++ InfraMap/InfraMap.Web.MVC/Models/EditaMaquinaModel.cs
@@ -0,0 +1,16 @@
+﻿using InfraMap.Dominio.Mesa.Maquina;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfraMap.Web.MVC.Models
+{
+    public class EditaMaquinaModel
+    {
+        public List<ModeloMaquina> Modelos { get; set; }
+
+        public ModeloMaquina ModeloEscolhido { get; set; }
+        public Maquina Maquina { get; set; }
+    }
+}
